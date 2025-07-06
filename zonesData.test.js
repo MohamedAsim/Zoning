9326ecc804bd41f03ec3068af6c8aaa36a836051
Mohamed A. Younis <20375550+MohamedAsim@users.noreply.github.com>
2025-07-06 23:10:33 +0300

    Update zones data from spreadsheet

--- zonesData.test.js
+++ zonesData.test.js
@@ -2,7 +2,7 @@ const zonesData = require('./zonesData');
 
 describe('zonesData statistics', () => {
   test('total number of zones', () => {
-    expect(zonesData.length).toBe(41);
+    expect(zonesData.length).toBe(42);
   });
 
   test('count per zone type', () => {
@@ -10,6 +10,6 @@ describe('zonesData statistics', () => {
       acc[z.type] = (acc[z.type] || 0) + 1;
       return acc;
     }, {});
-    expect(counts).toEqual({ PFZ: 8, SEZ: 4, Investment: 13, Private: 16 });
+    expect(counts).toEqual({ PFZ: 7, SEZ: 4, Investment: 13, Private: 18 });
   });
 });
